a6e7a701714bec3d0fb61bec853c35627f0e6d4a
kemsty <steeve.kemgne@orange.com>
2020-07-06 16:42:44 +0100

    Refactor BreadCrumb and sidebar, Add Views With Controllers

---
+++ StockManagement/Controllers/StocksController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StockManagement.Controllers
+{
+    public class StocksController : Controller
+    {
+        // GET: StockController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: StockController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+    }
+}
